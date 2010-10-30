64fe87f865d06f64e6c97e8259ebc65496dee49a
Aleksandr Furmanov <aleksandr.furmanov@gmail.com>
2010-10-29 21:46:41 -0500

    fixing initialization/loading order

--- lib/tagged_logger/railtie.rb
+++ lib/tagged_logger/railtie.rb
@@ -1,26 +1,24 @@
 if defined?(Rails::Railtie)
   module TaggedLogger
     class Railtie < Rails::Railtie
-      if TaggedLogger.options[:replace_existing_logger]
-        ActiveSupport.on_load(:action_controller) do
-          TaggedLogger.patch_logger(ActionController::Base, true).
-            patch_logger(ActionController::LogSubscriber, true).
-            rename [ActionController::LogSubscriber] => "action_controller.logsubscriber"
-        end
-        ActiveSupport.on_load(:active_record) do
-          TaggedLogger.patch_logger(ActiveRecord::Base, true).
-            patch_logger(ActiveRecord::LogSubscriber, true).
-            rename [ActiveRecord::LogSubscriber] => "active_record.logsubscriber"
-        end
-        ActiveSupport.on_load(:action_mailer) do
-          TaggedLogger.patch_logger(ActionMailer::Base, true).
-            patch_logger(ActionMailer::LogSubscriber, true).
-            rename [ActionMailer::LogSubscriber] => "action_mailer.logsubscriber"
-        end
-        ActiveSupport.on_load(:action_view) do
-          TaggedLogger.patch_logger(ActionView::LogSubscriber, true).
-            rename [ActionView::LogSubscriber] => "action_view.logsubscriber"
-        end
+      ActiveSupport.on_load(:action_controller) do
+        TaggedLogger.patch_logger(ActionController::Base, TaggedLogger.options[:replace_existing_logger]).
+          patch_logger(ActionController::LogSubscriber, TaggedLogger.options[:replace_existing_logger]).
+          rename [ActionController::LogSubscriber] => "action_controller.logsubscriber"
+      end
+      ActiveSupport.on_load(:active_record) do
+        TaggedLogger.patch_logger(ActiveRecord::Base, TaggedLogger.options[:replace_existing_logger]).
+          patch_logger(ActiveRecord::LogSubscriber, TaggedLogger.options[:replace_existing_logger]).
+          rename [ActiveRecord::LogSubscriber] => "active_record.logsubscriber"
+      end
+      ActiveSupport.on_load(:action_mailer) do
+        TaggedLogger.patch_logger(ActionMailer::Base, TaggedLogger.options[:replace_existing_logger]).
+          patch_logger(ActionMailer::LogSubscriber, TaggedLogger.options[:replace_existing_logger]).
+          rename [ActionMailer::LogSubscriber] => "action_mailer.logsubscriber"
+      end
+      ActiveSupport.on_load(:action_view) do
+        TaggedLogger.patch_logger(ActionView::LogSubscriber, TaggedLogger.options[:replace_existing_logger]).
+          rename [ActionView::LogSubscriber] => "action_view.logsubscriber"
       end
     end
   end
